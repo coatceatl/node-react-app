ab8c905f511c60619841adb8497a88e07b36f1b5
Oksana Tkachenko <oksana@crystax.net>
2017-10-23 17:08:30 +0300

    added webpack

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+//app.get('*', function(req,res) {
+//  res.sendFile(path.join(__dirname, '/public/index.html'));
+//});
+
 app.listen(3000, ()=> {
   console.log('Server started');
 });
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  PUBLIC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname,'src/js'),
+};
+
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'bundle.js',
+  },
+};
+
+
+
